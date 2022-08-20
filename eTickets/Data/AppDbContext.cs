40773ae93ee097e5618dfc45102a636c4b7c6b72
ddthilindra <59472990+ddthilindra@users.noreply.github.com>
2022-08-20 22:11:04 +0530

    DbContext file added

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext:DbContext
+    {
+        //ctor + TAB2 default constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+    }
+}
